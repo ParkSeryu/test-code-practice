301425bad7deae745dcb6d1f960a265b4395c3da
parkSeryu <pjh971212@gmail.com>
2024-07-21 19:19:03 +0900

    테스트 패키지 구조 변경

--- src/test/java/com/example/demo/user/service/UserServiceTest.java
+++ src/test/java/com/example/demo/user/service/UserServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -11,7 +11,6 @@ import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.domain.dto.UserCreate;
 import com.example.demo.user.domain.dto.UserUpdate;
 import com.example.demo.user.infrastructure.UserEntity;
-import com.example.demo.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
