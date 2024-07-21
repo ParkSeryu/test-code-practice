bc9bbae40d70fa5117a2c9f01071f154f29a2d72
parkSeryu <pjh971212@gmail.com>
2024-07-21 23:20:01 +0900

    Fake를 이용한 PostService 테스트 작성

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -3,9 +3,11 @@ package com.example.demo.post.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.demo.common.service.port.ClockHolder;
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
-import java.util.UUID;
+import java.time.Clock;
 import org.junit.jupiter.api.Test;
 
 public class PostTest {
@@ -18,6 +20,7 @@ public class PostTest {
                 .content("helloworld")
                 .build();
 
+        ClockHolder clockHolder = new TestClockHolder(1678530673958L);
         User writer = User.builder()
                 .email("kok202@naver.com")
                 .nickname("kok202")
@@ -27,16 +30,49 @@ public class PostTest {
                 .build();
 
         // when
-        Post post = Post.from(writer, postCreate);
+        Post post = Post.from(writer, postCreate, clockHolder);
 
         // then
         assertThat(post.getContent()).isEqualTo("helloworld");
+        assertThat(post.getCreatedAt()).isEqualTo(1678530673958L);
         assertThat(post.getWriter().getEmail()).isEqualTo("kok202@naver.com");
         assertThat(post.getWriter().getNickname()).isEqualTo("kok202");
         assertThat(post.getWriter().getAddress()).isEqualTo("Seoul");
         assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
         assertThat(post.getWriter().getCertificationCode()).isEqualTo("abcde");
+    }
 
+    @Test
+    public void PostUpdate으로_게시물을_수정할_수_있다() {
+        // given
+        PostUpdate postUpdate = PostUpdate.builder()
+                .content("byeWorld")
+                .build();
+
+        ClockHolder clockHolder = new TestClockHolder(16785306739522L);
+
+        User writer = User.builder()
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("abcde")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .content("helloworld")
+                .createdAt(1678530673958L)
+                .modifiedAt(0L)
+                .writer(writer)
+                .build();
+
+        // when
+        post = post.update(postUpdate, clockHolder);
+
+        // then
+        assertThat(post.getModifiedAt()).isEqualTo(16785306739522L);
+        assertThat(post.getContent()).isEqualTo("byeWorld");
 
     }
 
